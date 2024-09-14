ca1f79aa92098dff26c1c09588b6c21619f7143d
Kaustubh <kaustubhchaphekar@gmail.com>
2024-09-14 13:17:22 +0530

    update url

--- src/context/notes/NoteState.jsx
+++ src/context/notes/NoteState.jsx
@@ -4,7 +4,7 @@ import { NoteContext } from "./NoteContext";
 
 const NoteState = (props) => {
 
-  const host = "https://inotebook-vya3.onrender.com/"
+  const host = "https://inotebook-vya3.onrender.com"
   const [notes, setNotes] = useState([])
 
   //Get All Notes 
