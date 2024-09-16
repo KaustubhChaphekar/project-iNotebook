09421cd44160441bffea2162ceb8ae027944b327
Kaustubh <kaustubhchaphekar@gmail.com>
2024-09-16 11:04:39 +0530

    change routes

--- Backend/index.js
+++ Backend/index.js
@@ -1,15 +1,14 @@
 const connectToMongo = require("./db");
-const express = require('express');
-const cors = require('cors');
-const path = require('path');
+const express = require('express')
+const cors = require('cors')
 require('dotenv').config(); // Load environment variables from .env file
 
 connectToMongo();
-const app = express();
-const port = 3000;
+const app = express()
+const port = 3000
 
 const corsOptions = {
-  origin: process.env.REACT_APP_FRONTEND_URL, // Your frontend domain without the trailing slash
+  origin: 'https://project-inotebook.onrender.com', // Your frontend domain
   methods: ['GET', 'POST', 'PUT', 'DELETE'], // Allowed methods
   allowedHeaders: ['Content-Type', 'Authorization', 'auth-token'], // Allow auth-token header
 };
@@ -18,21 +17,18 @@ const corsOptions = {
 app.use(cors(corsOptions));
 
 // Middleware to parse incoming JSON requests
-app.use(express.json());
+app.use(express.json())
 
 // Routes
-app.use('/api/auth', require('./routes/auth'));
-app.use('/api/notes', require('./routes/notes'));
+app.use('/api/auth', require('./routes/auth'))
+app.use('/api/notes', require('./routes/notes'))
 
-// Serve static files from the 'dist' folder (assuming the React build is in the 'dist' directory)
-app.use(express.static(path.join(__dirname, 'dist')));
-
-// Catch-all route to serve index.html for client-side routes handled by React Router
-app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'dist', 'index.html')); 
-});
+// Default route
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
 
 // Start server
 app.listen(port, () => {
-  console.log(`iNotebook listening on port ${port}`);
-});
+  console.log(`iNotebook listening on port ${port}`)
+})
