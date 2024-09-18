bfd8d28192d69119fde0042a4ef618fda8218f2c
Kaustubh <kaustubhchaphekar@gmail.com>
2024-09-18 20:05:43 +0530

    fix 404 and db

--- Backend/db.js
+++ Backend/db.js
@@ -3,10 +3,7 @@ const mongoose = require('mongoose');
 const connectToMongo = async () => {
   try {
     const mongoURI = process.env.MONGODB_URI || "";
-    await mongoose.connect(mongoURI, {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    await mongoose.connect(mongoURI);
     console.log('Connected to MongoDB successfully');
   } catch (error) {
     console.error('Failed to connect to MongoDB:', error);
